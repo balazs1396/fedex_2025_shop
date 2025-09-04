f7e33b66d65cd6a06c5fca665a2b0b698b8cfcd5
Balazs Szedlacsek <balazs.szedlacsek@alzura.com>
2025-09-04 14:59:56 +0200

    Adds cart screen functionality
    Implements the cart screen with item display and removal features.
    Shows a message when the cart is empty.
    Includes a payment button placeholder.

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,18 +1,110 @@
+import 'package:fedex_2025_shop/models/product.dart';
+import 'package:fedex_2025_shop/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
   static String route = '/cart';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('My cart')),
-      body: Center(
-        child: const Text('This is my cart'),
+  void removeItemFromCart({
+    required BuildContext context,
+    required Product product,
+  }) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text("Remove this item to cart ?"),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Cancel"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+
+              context.read<Shop>().removeFromCart(item: product);
+            },
+            child: const Text("Yes"),
+          ),
+        ],
       ),
     );
   }
 
+  void onPay(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        content: Text(
+          "Thanks for your order",
+        ),
+      ),
+    );
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    final cart = context.watch<Shop>().cart;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Cart ")),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Expanded(
+            child: cart.isEmpty
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.shopping_cart, size: 120),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          "Your Cart is Empty",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                : ListView.builder(
+                    itemCount: cart.length,
+                    itemBuilder: (context, index) {
+                      final item = cart[index];
+
+                      return ListTile(
+                        title: Text(item.name),
+                        subtitle: Text(item.price.toStringAsFixed(2)),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.remove),
+                          onPressed: () {
+                            removeItemFromCart(context: context, product: item);
+                          },
+                        ),
+                      );
+                    },
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Container(
+              color: Colors.orange[300],
+              width: 500,
+              
+              child: IconButton(
+              onPressed: () {
+                onPay(context);
+              },
+              icon: Icon(Icons.payment),
+            )),
+          ),
+        ],
+      ),
+    );
+  }
 }
