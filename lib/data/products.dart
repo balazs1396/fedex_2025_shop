84200d50d968e351f98e862d29d1e1ff9f99a1e0
Balazs Szedlacsek <balazs.szedlacsek@alzura.com>
2025-09-04 14:03:56 +0200

    Adds product data and images for shop
    Initializes the product catalog with images and descriptions.
    
    This commit introduces the product model and a data source containing details for various items in the shop, enhancing the app's functionality by displaying product information.

---
+++ lib/data/products.dart
@@ -0,0 +1,46 @@
+import 'package:fedex_2025_shop/models/product.dart';
+
+final List<Product> products = [
+  Product(
+    name: "HANKOOK K425 Kinergy",
+    price: 24.99,
+    description:
+        "Die spritsparende Technologie des Kinergy eco basiert auf einer Kombination neuer Materialien wie Silica-Nanopartikeln unter Verwendung einer neuen Mischtechnologie. Diese Technologien steigern sowohl die Kraftstoffeffizienz als auch die Bremsleistung auf nasser Fahrbahn. Der Kinergy eco sorgt für den gewünschten Fahrkomfort und maximiert zugleich die Kraftstoffeffizienz. Diese Kombination aus Komfort und Effizienz wird durch die Antivibrationstechnologie von Hankook ermöglicht. Quelle: Hankook",
+    imagePath: "assets/images/products/tyre_1.jpg",
+  ),
+  Product(
+    name: "MOMO | REVENGE | Matt",
+    price: 124.99,
+    description:
+        "MOMO ist eine weltweit führende Marke in der Automobilen Zubehör-Industrie und sehr starkem Know-how auf dem Gebiet der Leichtmetallräder. Unser modernes befindet sich in Mailand. In enger Zusammenarbeit zwischen Designern und Ingenieuren geht es hier um die höchste Qualität und eine effiziente Produktionsleistung.",
+    imagePath: "assets/images/products/alloy_1.png",
+  ),
+  Product(
+    name: "RADAR RIVERA PRO 2",
+    price: 48.00,
+    description:
+        "Der asymmetrische Sommerreifen, außergewöhnliche Kontrolle und ausgezeichnete Haltbarkeit. Die ideale Wahl für kleine und mittlere Fahrzeuge.",
+    imagePath: "assets/images/products/tyre_2.jpg",
+  ),
+  Product(
+    name: "DELINTE DH2",
+    price: 49.00,
+    description:
+        "Das Profildesign in Kombination mit der Mittelrippe sorgt für gute Lenkbarkeit und Stabilität auf der Straße. Vier umlaufende Wasserkanäle tragen zu einer effektiven Wasserabführung bei und die Profilblockanordnung wurde für einen höheren Fahrkomfort und geringere Geräuschemissionen optimiert.",
+    imagePath: "assets/images/products/tyre_3.jpg",
+  ),
+  Product(
+    name: "TOLEDO TL1000",
+    price: 74.99,
+    description:
+        "Asymmetrisches Profildesign mit vier umlaufenden Rillen für eine gute Wasserableitung. Die Außenseite ist konstruiert um eine gute Stabilität und gutes Handling zu gewährleisten. Das Innenprofil leitet das Regenwasser ab und vermindert die Gefahr von Aquaplaning.",
+    imagePath: "assets/images/products/tyre_4.jpg",
+  ),
+  Product(
+    name: "AUTEC | TALLIN (TL) | Titansilber",
+    price: 119.99,
+    description:
+        "Die AUTEC GmbH & Co. KG etablierte sich in den letzten 20 Jahren zu einem Gütezeichen hochwertiger und optisch ansprechender Leichtmetallräder. AUTEC-Räder zeichnen sich durch innovative und kreative Designs, ein hohes Qualitätsniveau, eine breite Produktpalette sowie eine hohe Anwendungsvielfalt aus. ABE´s und TÜV-Gutachten sind für nahezu alle Fahrzeuge vorhanden",
+    imagePath: "assets/images/products/alloy_2.png",
+  ),
+];
--- lib/shop.dart
+++ lib/shop.dart
@@ -13,41 +13,24 @@ class ShopScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Fedex days 2025 - Shop'),),
+      appBar: AppBar(title: const Text('Fedex days 2025 - Shop')),
       body: ListView(
         children: [
-          SizedBox(
-            height: Utils.getScreenHeight(context) * 0.025,
-          ),
-          Padding(
-            padding:
-                EdgeInsets.only(bottom: Utils.getScreenHeight(context) * 0.015),
-            child: Center(
-              child: Text(
-                "Pick from a Selected list of premium products ",
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.inversePrimary),
+          SizedBox(height: Utils.getScreenHeight(context) * 0.025),
+          ScrollConfiguration(
+            behavior: MyCustomScrollBehavior(),
+            child: SizedBox(
+              height: Utils.getScreenHeight(context) * 0.6,
+              child: ListView.builder(
+                controller: controller,
+                scrollDirection: Axis.horizontal,
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return MyProductTile(product: products[index]);
+                },
               ),
             ),
           ),
-          ScrollConfiguration(
-              behavior: MyCustomScrollBehavior(),
-              child: SizedBox(
-                height: Utils.getScreenHeight(context) * 0.68,
-                child: ListView.builder(
-                  controller: controller,
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.symmetric(
-                      horizontal: Utils.getScreenWidth(context) * 0.03,
-                      vertical: Utils.getScreenWidth(context) * 0.005),
-                  itemCount: products.length,
-                  itemBuilder: (context, index) {
-                    final product = products[index];
-                    return MyProductTile(product: product);
-                  },
-                ),
-              ))
         ],
       ),
     );
